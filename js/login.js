5de5ee28ad01ff7f7a1c55b88ac9176014f773aa
faham112 <159237736+faham112@users.noreply.github.com>
2025-04-24 09:11:44 +0500

    Fix: Initialize Firebase app in login.js

--- js/login.js
+++ js/login.js
@@ -1,3 +1,17 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC_RFY98xrBfLpYz3c_zw-fJc6pieGcA88",
+  authDomain: "webappfinal112.firebaseapp.com",
+  projectId: "webappfinal112",
+  storageBucket: "webappfinal112.firebasestorage.app",
+  messagingSenderId: "82896269063",
+  appId: "1:82896269063:web:37ecddbe1ab34407675fa2",
+  measurementId: "G-XV090SK0VK"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 const auth = firebase.auth();
 
 function isValidEmail(email) {
